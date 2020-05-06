718a964ad8228aa1eb2f26530def8c99cb47c1de
Amir Girgis <amir.amgad01@gmail.com>
2020-05-06 18:30:30 +0400

    updated choropleth map to put country text above tiles

--- Visualizations/edit_geojsondata.py
+++ Visualizations/edit_geojsondata.py
@@ -5,12 +5,12 @@ seed(1)
 count = 0
 countries = []
 data = {}
-with open('meningitis_belt.json', "r") as json_file:
+with open('meningitis_belt.geojson', "r") as json_file:
 	data = json.load(json_file)
 	for feature in data["features"]:
 		print(feature["properties"])
 		feature["properties"]["density"] = randint(0, 100)
 
 
-with open("men_features.js", "w") as json_file:
+with open("men_featuresv2.js", "w") as json_file:
 	json.dump(data, json_file, sort_keys = True, indent=1)
